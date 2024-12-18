1c65e12767b7952f3a2c64be7e3f9aa31eaf971f
B.Madhusri <149278991+bmadhusri@users.noreply.github.com>
2024-12-18 06:42:09 +0530

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Main {
+public class QuizAppApplication {
     public static void main(String[] args) {
         SpringApplication.run(QuizAppApplication.class, args);
     }
